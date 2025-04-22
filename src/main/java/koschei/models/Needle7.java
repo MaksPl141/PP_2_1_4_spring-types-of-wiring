56fee8d11f093bdd2749ee55795e9f00e6eb21b5
maksim <rv.maks@mail.ru>
2025-04-22 19:06:22 +0300

    Сделал изменение

--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -14,6 +14,6 @@ public class Needle7 {
     }
 
     @Override
-    public String toString() {return " " + deth.toString();
+    public String toString() {return "смерть Кощея на игле :(" + deth.toString();
     }
 }
